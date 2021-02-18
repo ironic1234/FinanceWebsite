435b7b7141c840a09cd836c38a06a4df00dcdccc
Ronak Jain <rpjain06@gmail.com>
2021-02-18 09:37:14 -0500

    hopefully the website works :)

--- Website/static/data.js
+++ Website/static/data.js
@@ -1,18 +1,27 @@
-fetch("http://192.168.1.179:4000/http://192.168.1.179:3000/data")
-.then((resp) => document.body.innerHTML = await getTextFromStream(resp.body))
+async function fetchTest() {
+	let response = await fetch('http://192.168.1.179:4000/http://192.168.1.179:3000/data');
+	let responseText = await getTextFromStream(response.body);
+
+	document.body.innerHTML = responseText;
+}
 
 async function getTextFromStream(readableStream) {
-	    let reader = readableStream.getReader();
-	    let utf8Decoder = new TextDecoder();
-	    let nextChunk;
-	    
-	    let resultStr = '';
-	    
-	    while (!(nextChunk = await reader.read()).done) {
-		            let partialData = nextChunk.value;
-		            resultStr += utf8Decoder.decode(partialData);
-		        }
-	    
-	    return resultStr;
+	let reader = readableStream.getReader();
+	let utf8Decoder = new TextDecoder();
+	let nextChunk;
+
+	let resultStr = '';
+
+	while (!(nextChunk = await reader.read()).done) {
+		let partialData = nextChunk.value;
+		resultStr += utf8Decoder.decode(partialData);
+	}
+
+	return resultStr;
 }
+
+(async() => {
+	await fetchTest();
+})();
+
 
