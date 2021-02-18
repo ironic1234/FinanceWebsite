2db4344ce5baa7dfd348df64f4d66815f0ee0dab
Ronak Jain <rpjain06@gmail.com>
2021-02-18 14:33:38 +0000

    working on the website

--- Website/static/data.js
+++ Website/static/data.js
@@ -1,2 +1,18 @@
-let response = fetch('http://192.168.1.179:4000/http://192.168.1.179:3000/data')
-console.log(response.text)
+fetch("http://192.168.1.179:4000/http://192.168.1.179:3000/data")
+.then((resp) => document.body.innerHTML = await getTextFromStream(resp.body))
+
+async function getTextFromStream(readableStream) {
+	    let reader = readableStream.getReader();
+	    let utf8Decoder = new TextDecoder();
+	    let nextChunk;
+	    
+	    let resultStr = '';
+	    
+	    while (!(nextChunk = await reader.read()).done) {
+		            let partialData = nextChunk.value;
+		            resultStr += utf8Decoder.decode(partialData);
+		        }
+	    
+	    return resultStr;
+}
+
